23bd7d671db2c19c32c88733ca50e9f069e3a9dc
x-easy <mosp.github.io@gmail.com>
2026-01-25 02:53:01 +0800

    创建 stack.js

---
+++ assets/js/stack.js
@@ -0,0 +1,21 @@
+// assets/js/stack.js
+const stack = document.getElementById("stack");
+const pages = [];
+
+export function pushPage(el) {
+  el.className = "page enter";
+  stack.appendChild(el);
+  pages.push(el);
+
+  requestAnimationFrame(() => {
+    el.classList.add("active");
+  });
+}
+
+export function popPage() {
+  if (pages.length <= 1) return;
+
+  const page = pages.pop();
+  page.classList.remove("active");
+  page.addEventListener("transitionend", () => page.remove(), { once: true });
+}
