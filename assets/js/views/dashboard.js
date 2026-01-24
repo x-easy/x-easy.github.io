665fee9290f1f2f96dea6f030e130ab50879df87
x-easy <mosp.github.io@gmail.com>
2026-01-25 03:24:53 +0800

    更新 dashboard.js

--- assets/js/views/dashboard.js
+++ assets/js/views/dashboard.js
@@ -3,7 +3,7 @@ export function render(root) {
   root.innerHTML = `
     <div style="padding:16px">
       <h2>Dashboard</h2>
-      <p>App Shell 正常運作中。</p>
+      <p>這代表整個 App Shell 已正常運作。</p>
     </div>
   `;
 }
