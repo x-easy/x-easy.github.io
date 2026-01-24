1224be746f57cdd38bb4f366e7f49b208c0003ea
x-easy <mosp.github.io@gmail.com>
2026-01-25 03:07:32 +0800

    创建 dashboard.js

---
+++ assets/js/views/dashboard.js
@@ -0,0 +1,9 @@
+// assets/js/views/dashboard.js
+export function render(root) {
+  root.innerHTML = `
+    <div style="padding:16px">
+      <h2>Dashboard</h2>
+      <p>App Shell 正常運作中。</p>
+    </div>
+  `;
+}
