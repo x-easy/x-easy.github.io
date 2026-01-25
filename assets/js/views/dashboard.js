50b5c33b709c27186c665748860fdc318e949de3
x-easy <mosp.github.io@gmail.com>
2026-01-25 20:46:11 +0800

    删除 dashboard.js

--- assets/js/views/dashboard.js
+++
@@ -1,9 +0,0 @@
-// assets/js/views/dashboard.js
-export function render(root) {
-  root.innerHTML = `
-    <div style="padding:16px">
-      <h2>Dashboard</h2>
-      <p>這代表整個 App Shell 已正常運作。</p>
-    </div>
-  `;
-}
