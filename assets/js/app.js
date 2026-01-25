ae87bdca239cf05d7f6b117ce8260f1605b5ef29
x-easy <mosp.github.io@gmail.com>
2026-01-26 01:52:21 +0800

    创建 app.js

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.tab').forEach(tab => {
+  tab.addEventListener('click', () => {
+    document.querySelectorAll('.tab')
+      .forEach(t => t.classList.remove('active'));
+    tab.classList.add('active');
+  });
+});
