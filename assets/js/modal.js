0ad636ac80da6d95b86dab6abff5b9d2d733cca9
x-easy <mosp.github.io@gmail.com>
2026-01-26 16:54:32 +0800

    Delete assets directory

--- assets/js/modal.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const modal = document.getElementById('musicModal');
-  const close = document.getElementById('closeModal');
-
-  if (!modal || !close) return;
-
-  close.onclick = () => modal.classList.remove('active');
-})();
--- assets/js/player.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  const btn = document.getElementById('playBtn');
-  if (!btn) return;
-
-  btn.addEventListener('click', () => {
-    document.getElementById('musicModal')?.classList.add('active');
-  });
-})();
